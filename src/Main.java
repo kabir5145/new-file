3715470d2d93a9a1870b05ede613cd1ae53b9e85
kabir5145 <kabir10112006@gmail.com>
2025-10-03 08:40:59 +0530

    java question

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.print("sum of the numbers is:");
+        System.out.print("average of the numbers is:");
         int num1 =78;
         int num2 =2;
-        int sum = num1+num2;
-        System.out.println(sum);
+        int avg = (num1+num2)/2;
+        System.out.println(avg);
         }
     }
